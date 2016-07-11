d37f88aca2311a26ff818925d8394d7b75204e79
WorkHyeop <WorkHyeop>
2016-07-11 20:21:18 +0900

    init

---
+++ HuntingBear/Assets/02.Scripts/Logo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class Logo : MonoBehaviour {
+    float time = 0.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        if (time >= 3.0f)
+        {
+            SceneManager.LoadScene(1);
+        }
+	}
+}
